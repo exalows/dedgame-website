e4156763c8f7656d4301bdefe94234c94c6b2ba7
unknown <sk02scsa@gmail.com>
2022-01-02 15:28:56 +0530

    Added

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './homepage.styles.scss'
+
+
+const HomePage = (props) => (
+    <div className="homepage">
+        <Directory  />
+    </div>
+)
+
+export default HomePage
