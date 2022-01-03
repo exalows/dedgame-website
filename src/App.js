dbd2a101060805e63b2de64b42b5a0d3a8c1e01e
unknown <sk02scsa@gmail.com>
2022-01-03 11:44:58 +0530

    Added Shop Page, Collecitons and Collection Items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path='/' element={<HomePage/>} />
+        <Route exact path='/shop' element={<ShopPage/>} />
       </Routes>
     </div>
   );
